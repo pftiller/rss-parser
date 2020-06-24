12958dc82dc07d4a6f3923236d8891cad06442ce
Tiller <ptiller@mpr.org>
2020-06-24 11:22:06 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+let Parser = require('./index');
+
+const PORT = process.env.PORT || 3001;
+
+
+
+app.listen(PORT, (error) => {
+    console.log("listening on " + PORT + "...");
+});
