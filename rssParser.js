121a90b24c6ae54c2eadeed1154b6e1d88120a87
Paul Tiller <ptiller@mpr.org>
2023-04-19 13:57:56 -0500

    adding Financially Inclined

--- rssParser.js
+++ rssParser.js
@@ -4,68 +4,8 @@ const {BigQuery} = require('@google-cloud/bigquery');
 let parser = new Parser();
 const urls = [
     {
-        program: "This Is Uncomfortable",
-        feed: "https://www.marketplace.org/feed/podcast/this-is-uncomfortable-reema-khrais"
-    },
-    {
-        program: "Million Bazillion",
-        feed: "https://www.marketplace.org/feed/podcast/million-bazillion"
-    },
-    {
-        program: "The Uncertain Hour",
-        feed: "https://www.marketplace.org/feed/podcast/the-uncertain-hour"
-    },
-    {
-        program: "Julie's Library",
-        feed: "https://feeds.publicradio.org/public_feeds/julies-library/rss/rss"
-    },
-    {
-        program: "Don't Ask Tig",
-        feed: "https://feeds.publicradio.org/public_feeds/dont-ask-tig/rss/rss.rss"
-    },
-    {
-        program: "In the Dark",
-        feed: "https://feeds.publicradio.org/public_feeds/in-the-dark/rss/rss"
-    },
-    {
-        program: "Terrible, Thanks for Asking",
-        feed: "https://feeds.publicradio.org/public_feeds/terrible-thanks-for-asking/rss/rss.rss"
-    },
-    {
-        program: "Spectacular Failures",
-        feed: "https://feeds.publicradio.org/public_feeds/spectacular-failures/rss/rss.rss"
-    },
-    {
-        program: "The Slowdown",
-        feed: "https://feeds.publicradio.org/public_feeds/the-slowdown/rss/rss.rss"
-    },
-    {
-        program: "The Splendid Table",
-        feed: "https://feeds.publicradio.org/public_feeds/the-splendid-table/rss/rss.rss"
-    },
-    {
-        program: "Brains On!",
-        feed: "https://feeds.publicradio.org/public_feeds/brains-on/rss/rss.rss"
-    },
-    {
-        program: "Field Work",
-        feed: "https://feeds.publicradio.org/public_feeds/fieldwork/rss/rss.rss"
-    },
-    {
-        program: "Smash Boom Best",
-        feed: "https://feeds.publicradio.org/public_feeds/smash-boom-best/rss/rss.rss"
-    },
-    {
-        program: "TBTL",
-        feed: "https://feeds.publicradio.org/public_feeds/tbtl/rss/rss.rss"
-    },
-    {
-        program: "TBTL",
-        feed: "https://feeds.publicradio.org/public_feeds/tbtl/rss/rss.rss"
-    },
-    {
-        program: "In Front of Our Eyes",
-        feed: "https://feeds.publicradio.org/public_feeds/in-front-of-our-eyes/rss/rss.rss"
+        program: "Financially Inclined",
+        feed: "https://www.marketplace.org/feed/podcast/financially-inclined"
     }
 ];
 const projectId = `apmg-data-warehouse`;
@@ -95,16 +35,13 @@ let removeDups = async () => {
 let parseRSS = (url) => {
     return new Promise((resolve, reject) => {
         let dataToAdd = [];
-        let parseUri = new RegExp('rss\/o(.*)');
+        let parseUri = new RegExp('\/o(,?.*)');
         function createRecord(url, item) {
             return {
                 program: url.program,
                 title: item.title,
-                uri_path: null,
-                episode: moment(item.pubDate).format('YYYY-MM-DD'),
-            //     getUri() {
-            //         return this.uri_path = parseUri.exec(item.enclosure.url);
-            //     }
+                uri_path: parseUri.exec(item.enclosure.url)[1],
+                episode: moment(item.pubDate).format('YYYY-MM-DD')
             }
         }
         parser.parseURL(url.feed, (err, feed) => {
@@ -114,9 +51,6 @@ let parseRSS = (url) => {
                 feed.items.forEach(item => {
                     if (item.hasOwnProperty('enclosure')) {
                         var obj = createRecord(url, item);
-                        // var obj2 = obj.getUri();
-                        // obj.uri_path = obj2[1];
-                        // delete obj.getUri;
                         dataToAdd.push(obj)
                     }
                     resolve(dataToAdd);
